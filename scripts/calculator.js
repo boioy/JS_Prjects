d7f150081c5907c05c71eb6beace117c0e9bef9d
Abbas Bachir <abbas.bachir@hotmail.de>
2024-03-22 02:53:30 +0100

    Math Error, wenn 1. Element * || / onCLick eventListener komplett über js backspace-button

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,4 +1,23 @@
 let calculation = '';
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach((buttonElement, index) => {
+  buttonElement.addEventListener('click', () => {
+    let char = buttonElement.innerHTML;
+    if (char === '='){
+      calcResult();
+    } else if (char === 'C'){
+      backspace();
+    } else if (char === 'Clear'){
+      clearCalc();
+    }
+    else{
+      addToCalc(char);
+    }
+    
+  });
+});
       
 function addToCalc(char){
   calculation += char;
@@ -6,10 +25,24 @@ function addToCalc(char){
 }
 
 function calcResult(){
+  if (calculation.substring(0,1) === '*' || calculation.substring(0,1) === '/'){
+    document.querySelector('.js-display-result').innerHTML = 'Math Error';
+    console.log(`${calculation} => Math Error`);
+    calculation = '';
+  }
   const result = eval(calculation);
-  document.querySelector('.js-display-result').innerHTML = result;
-  console.log(`${calculation} = ${result}`);
-  calculation = '';
+  if (result != undefined){
+    document.querySelector('.js-display-result').innerHTML = result;
+    console.log(`${calculation} = ${result}`);
+    calculation = '';
+  }
+  
+}
+
+function backspace(){
+  calculation = calculation.slice(0,-1);
+  console.log(calculation);
+  document.querySelector('.js-display-calc').innerHTML = calculation;
 }
 
 function clearCalc(){
@@ -17,3 +50,4 @@ function clearCalc(){
   document.querySelector('.js-display-calc').innerHTML = ''
   document.querySelector('.js-display-result').innerHTML = ''
 }
+
